682cdc191504b6507d88f962e51eef2d2681fbc1
George Paloulian <george.k.paloulian@gmail.com>
2024-11-18 21:40:35 -0800

    metal lb install and config

--- setup.sh
+++ setup.sh
@@ -11,3 +11,42 @@ kubectl label nodes ubuntu-rock1 kubernetes.io/role=worker
 kubectl label nodes ubuntu-rock2 kubernetes.io/role=worker
 kubectl label nodes ubuntu-rock3 kubernetes.io/role=worker
 kubectl label nodes ubuntu-rock4 kubernetes.io/role=worker
+
+############################################################
+# Install Helm - ONLY ON MASTER NODE - ubuntu-rock1
+# Fix kubeconfig file to prevent Helm errors
+export KUBECONFIG=~/.kube/config
+mkdir ~/.kube 2> /dev/null
+sudo k3s kubectl config view --raw > "$KUBECONFIG"
+chmod 600 "$KUBECONFIG"
+echo "KUBECONFIG=$KUBECONFIG" >> /etc/environment
+
+# Create a directory for Helm and navigate into it
+mkdir ~/helm && cd helm
+# Download Helm installer
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+# Modify permissions for execution
+chmod 700 get_helm.sh
+# Install Helm
+./get_helm.sh
+
+# Verify Helm installation
+helm version
+############################################################
+
+
+############################################################
+# Install MetalLB - https://metallb.universe.tf/
+# Add MetalLB repository to Helm
+helm repo add metallb https://metallb.github.io/metallb
+
+# Check the added repository
+helm search repo metallb
+
+# Install metallb with helm
+helm upgrade --install metallb metallb/metallb --create-namespace --namespace metallb-system --wait
+
+# Assign IP range for MetalLB from yaml file
+kubectl apply -f metallb.yaml
+
+############################################################
